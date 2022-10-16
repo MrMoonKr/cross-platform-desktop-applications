74f83b35e7e7adbaaa806bbb09e00ea4e636626d
MrMoonKr <MrMoonKr@gmail.com>
2022-10-17 01:24:32 +0900

    contextIsolation: false 추가

--- chapter-02/lorikeet-electron/main.js
+++ chapter-02/lorikeet-electron/main.js
@@ -13,7 +13,7 @@ app.on('window-all-closed',() => {
 app.on('ready', () => {
   // Extra configuration settings added due to issue, see 
   // https://github.com/paulbjensen/cross-platform-desktop-applications/issues/9
-  mainWindow = new BrowserWindow({ webPreferences: { nodeIntegration: true } });
+  mainWindow = new BrowserWindow({ webPreferences: { nodeIntegration: true, contextIsolation: false } });
   mainWindow.loadURL(`file://${app.getAppPath()}/index.html`);
   mainWindow.on('closed', () => { mainWindow = null; });
 });
