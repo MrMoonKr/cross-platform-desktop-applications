883d6d65251efb277cb057ea0ed9dbbee182dbaa
Mr.Moon <MrMoonKr@gmail.com>
2023-09-26 18:44:51 +0900

    JSDoc 문서화 테스트...

--- chapter-02/lorikeet-nwjs/app.js
+++ chapter-02/lorikeet-nwjs/app.js
@@ -1,22 +1,32 @@
 
-// ES6 가 동작하긴 하는데...
+// html에 임베드 되는 코드는 ES6 가 동작하긴 하는데...
 // 외부모듈 사용시는 CommonJS 형태로 사용해야 함. './js/filebrowser.js' 참고할 것
 import FileBrowser from './js/filebrowser.js' ;
 
+/**
+ * 'main-area' 노드에 파일 정보 노드 생성 및 추가
+ * @param { { file: string, path: string, type: string } } file 파일관련정보
+ */
+function displayFile( file ) {
 
-function displayFile( file ) 
-{
-    const mainArea = document.getElementById( 'main-area' );
-    const template = document.querySelector( '#item-template' );
-    let clone = document.importNode( template.content, true );
-    clone.querySelector( 'img' ).src = `images/${file.type}.svg`;
-    clone.querySelector( '.filename' ).innerText = file.file;
+    const mainArea  = document.getElementById( 'main-area' );
+
+    const template  = document.querySelector( '#item-template' );   // id로 검색
+    let clone       = document.importNode( template.content, true );
+    clone.querySelector( 'img' ).src = `images/${file.type}.svg`;   // tag로 검색
+    clone.querySelector( '.filename' ).innerText = file.file;       // class로 검색
+
     mainArea.appendChild( clone );
 }
 
+/**
+ * 
+ * @param {NodeJS.ErrnoException | null} err 실행에러
+ * @param {{ file: string, path: string, type: string }[]} files 특정 디렉토리내 파일경로들
+ * @returns 
+ */
+function displayFiles( err, files ) {
 
-function displayFiles( err, files ) 
-{
     if ( err ) 
     {
         return alert( 'Sorry, we could not display your files' );
@@ -30,6 +40,7 @@ function main()
     let homeFolder = FileBrowser.getUsersHomeFolder() ;
 
     FileBrowser.getFilesInFolder( homeFolder, ( err, files ) => {
+        
         if ( err ) 
         {
             return alert( 'Sorry, we could not load your home folder' );
@@ -48,17 +59,6 @@ function main()
         const currentFolder = document.getElementById('current-folder');
         currentFolder.innerText = FileBrowser.getUsersHomeFolder();
     });
-
-    // let homeFolder = FileBrowser.getUsersHomeFolder() ;
-
-    // FileBrowser.getFilesInFolder( homeFolder, ( err, files ) => {
-    //     if ( err ) 
-    //     {
-    //         return alert( 'Sorry, we could not load your home folder' );
-    //     }
-
-    //     FileBrowser.inspectAndDescribeFiles( homeFolder, files, displayFiles );
-    // } );
 }
 
 main();
--- chapter-02/lorikeet-nwjs/js/filebrowser.js
+++ chapter-02/lorikeet-nwjs/js/filebrowser.js
@@ -13,16 +13,28 @@ const async = require( 'async' ) ;
 
 class FileBrowser
 {
+    /**
+     * osenv 모듈을 이용한 사용자 홈 디렉토리 경로 조회
+     * @returns 사용자 홈 디렉토리 경로
+     */
     static getUsersHomeFolder() 
     {
         return osenv.home() ;
     }
-    
+    /**
+     * fs 모듈을 이용한 디렉토리 읽기
+     * @param {string} folderPath 
+     * @param {( err: NodeJS.ErrnoException | null, files: string[] ) => void} cb 
+     */
     static getFilesInFolder( folderPath, cb ) 
     {
         fs.readdir( folderPath, cb ) ;
     }
-    
+    /**
+     * 
+     * @param {string} filePath 
+     * @param {( err: NodeJS.ErrnoException | null, result: {file: string, path: string, type: string} ) => void} cb 
+     */
     static inspectAndDescribeFile( filePath, cb ) 
     {
         let result = {
@@ -51,11 +63,17 @@ class FileBrowser
             }
         } ) ;
     }
-    
-    static inspectAndDescribeFiles( folderPath, files, cb )
-    {
+    /**
+     * 
+     * @param {string} folderPath 
+     * @param {string[]} files 
+     * @param {( err: NodeJS.ErrnoException | null, files: { file: string, path: string, type: string }[] ) => void} cb 
+     */
+    static inspectAndDescribeFiles( folderPath, files, cb ) {
+
         async.map( files, 
             ( file, asyncCB ) => {
+
                 let resolvedFilePath = path.resolve( folderPath, file ) ;
     
                 FileBrowser.inspectAndDescribeFile( resolvedFilePath, asyncCB ) ;
